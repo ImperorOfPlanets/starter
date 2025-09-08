57d960201349936992198722fd777f2ac81a3125
adminuser <glok87@list.ru>
2025-09-08 16:09:27 +0500

    фывфы

--- starter_files/updates/extracted/docker/20250908_105017/configs/init/start-supervisor.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Упрощенная версия без изменения прав
-mkdir -p /var/log/supervisor
-mkdir -p /var/log/worker
-
-# Запуск Supervisor
-exec /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
--- starter_files/updates/extracted/docker/20250908_110530/configs/init/start-horizon.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Ожидаем готовности приложения
-while [ ! -f /var/www/html/ready.txt ]; do
-    echo "Ожидаем появления /var/www/html/ready.txt..."
-    sleep 5
-done
-
-exec php artisan horizon
